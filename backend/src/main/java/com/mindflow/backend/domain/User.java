15e236387104f455b0259cafc2abc8b32dc94a8f
Aleazar Wolde <aleazarali123@gmail.com>
2025-07-15 20:54:55 -0400

    feat(domain): created User class with id, name, email, and password fields using JPA annotations like @Entity, @Id, @GeneratedValue, and @Column

--- backend/src/main/java/com/mindflow/backend/domain/User.java
+++ backend/src/main/java/com/mindflow/backend/domain/User.java
@@ -1,12 +1,59 @@
 package com.mindflow.backend.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import org.hibernate.validator.constraints.UUID;
 
-@Entity //  An entity annotation creates a table in the DataBase for Us.
+
+@Entity //  An entity annotation creates a table in the DataBase for Us. JPA
+@Table (name ="users") //The table annotation gives us the choice to change the table name that is created in the Db.
 public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
 
+    public User(String name, UUID id, String email, String password){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
 
-    //private String name;
+    // Getters and Setters
 
+    //Getter and setter for the User filed.
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    //Getter and setter for the id filed
+    public UUID getId(){
+        return id;
+    }
+    public void setId(UUID id){
+        this.id = id;
+    }
+
+    //Getter and setter for the email filed
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    //Getter and Setter for the password filed
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
 
 }
