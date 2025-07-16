0fa2fcb13158f3afd213c126c10f5ac4e5b219f9
Aleazar Wolde <aleazarali123@gmail.com>
2025-07-15 20:57:43 -0400

    I created a TasRepository interface class to a sign what the Task class need to do.

---
+++ backend/src/main/java/com/mindflow/backend/repository/TaskRepository.java
@@ -0,0 +1,12 @@
+package com.mindflow.backend.repository;
+
+import com.mindflow.backend.domain.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+import java.util.List;
+import java.time.LocalDate;
+
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+}
