c3a9c8826b7f689708a1d4d9a9f1dc519b751f14
Aleazar Wolde <aleazarali123@gmail.com>
2025-07-15 20:11:34 -0400

    I created a User class.

--- backend/src/main/java/com/mindflow/backend/domain/User.java
+++ backend/src/main/java/com/mindflow/backend/domain/User.java
@@ -1,6 +1,12 @@
 package com.mindflow.backend.domain;
 
+import jakarta.persistence.Entity;
+
+@Entity //  An entity annotation creates a table in the DataBase for Us.
 public class User {
-    private String name;
+
+
+    //private String name;
+
 
 }
