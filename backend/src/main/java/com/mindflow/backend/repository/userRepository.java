8cc45279b5e4166f3b68ee4171092b8b9cf15207
Aleazar Wolde <aleazarali123@gmail.com>
2025-07-16 20:53:45 -0400

    I created a userRepository interface class to a sign what the User class need to do.

--- backend/src/main/java/com/mindflow/backend/repository/userRepository.java
+++ backend/src/main/java/com/mindflow/backend/repository/userRepository.java
@@ -1,4 +1,7 @@
 package com.mindflow.backend.repository;
 
-public interface userRepository {
+import com.mindflow.backend.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface userRepository extends JpaRepository<User, Long> {
 }
