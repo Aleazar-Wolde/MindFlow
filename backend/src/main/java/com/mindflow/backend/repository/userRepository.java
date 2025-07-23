b275bfd7942e9f72de6ea6ce3e73bf8d9359f7ae
Aleazar Wolde <aleazarali123@gmail.com>
2025-07-22 20:59:27 -0400

    I am created on method in the userRepository class

--- backend/src/main/java/com/mindflow/backend/repository/userRepository.java
+++ backend/src/main/java/com/mindflow/backend/repository/userRepository.java
@@ -2,6 +2,12 @@ package com.mindflow.backend.repository;
 
 import com.mindflow.backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface userRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface userRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
