2d8cc25bbd2b1bb1f714e198d2fe170bd3e5a314
Aleazar Wolde <aleazarali123@gmail.com>
2025-07-22 21:16:51 -0400

    a method to get a user by an email.

--- backend/src/main/java/com/mindflow/backend/service/userService.java
+++ backend/src/main/java/com/mindflow/backend/service/userService.java
@@ -2,6 +2,10 @@ package com.mindflow.backend.service;
 
 import org.apache.catalina.User;
 
+import java.util.Optional;
+
 public class userService {
     User registerUser(User user);
+
+    Optional<User> getUserByEmail(String email);
 }
