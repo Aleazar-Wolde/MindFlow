6ff62414eff415e1f9eabaedddd2103a8c768fc9
Aleazar Wolde <aleazarali123@gmail.com>
2025-07-31 10:59:27 -0400

    I changed the structures of the fields

--- backend/src/main/java/com/mindflow/backend/repository/userRepository.java
+++ backend/src/main/java/com/mindflow/backend/repository/userRepository.java
@@ -1,13 +1,13 @@
-package com.mindflow.backend.repository;
-
-import com.mindflow.backend.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-import java.util.UUID;
-
-@Repository
-public interface userRepository extends JpaRepository<User, UUID> {
-    <User> Optional<User> findByEmail(String email);
-}
+//package com.mindflow.backend.repository;
+//
+//import com.mindflow.backend.domain.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//import java.util.UUID;
+//
+//@Repository
+//public interface userRepository extends JpaRepository<User, UUID> {
+//    <User> Optional<User> findByEmail(String email);
+//}
