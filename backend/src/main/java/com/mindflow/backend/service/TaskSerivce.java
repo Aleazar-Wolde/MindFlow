70745c80f3e71e5b4cc6f36a9e808fc78dbce097
Aleazar Wolde <aleazarali123@gmail.com>
2025-08-06 16:19:34 -0400

    feat: add TaskService class with methods for create, read, update, delete; inject TaskRepository via constructor

--- backend/src/main/java/com/mindflow/backend/service/TaskSerivce.java
+++ backend/src/main/java/com/mindflow/backend/service/TaskSerivce.java
@@ -1,7 +1,43 @@
 package com.mindflow.backend.service;
 
 import com.mindflow.backend.repository.TaskRepository;
+import org.springframework.scheduling.config.Task;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class TaskSerivce implements TaskRepository {
+    private final TaskRepository taskRepository;
 
+    public TaskSerivce(TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    public Task getTask(UUID id){
+        return taskRepository.findById(id).orElseThrow(() -> new RuntimeException("task not found"));
+    }
+    public Task createTask(Task task){
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(UUID id, Task updatedTask){
+        Task task = getTask(id);
+
+        task.setTitle(updatedTask.getTitle());
+        task.setDescription(updatedTask.getDescription());
+        task.setDueDate(updatedTask.getDueDate());
+        task.setCompleted(updatedTask.isCompleted());
+
+        return taskRepository.save(task);
+    }
+
+    public void deletTask(UUID id){
+        taskRepository.deleteById(id);
+    }
 }
